c1a91342c798f0eb61d9262e168b32ca76cfc3d2
Rehman-codes <abdurrehman.swe@gmail.com>
2025-02-16 07:28:38 +0500

    dummy UI complete

--- wordladder/app/urls.py
+++ wordladder/app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mode/', views.mode, name='mode'),
+    path('challenge/', views.challenge, name='challenge'),
+    path('play/<int:challenge_id>/', views.playground, name='playground'),
+
 ]
--- wordladder/app/views.py
+++ wordladder/app/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, "base.html")
 
+def mode(request):
+    return render(request, "mode.html")
+
+def challenge(request):
+    return render(request, "challenge.html")
+
+def playground(request, challenge_id):
+    return render(request, "playground.html")
